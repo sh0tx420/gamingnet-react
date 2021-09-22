210564d54118c27a0ed8b1e578204aaba8e67e16
sh0tx420 <sh0tx.cosmoline@gmail.com>
2021-09-22 20:06:05 +0300

    Added CSS overrides

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 import Home from "./Home";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
-ReactDOM.render(<Home/>, document.querySelector("#app"));
+
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+        background: {
+            default: "#222222"
+        }
+    },
+});
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Home/>
+    </ThemeProvider>,
+    document.querySelector("#app")
+);
