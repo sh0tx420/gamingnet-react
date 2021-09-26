75663c7e0f5557660cb23a4164e2679384f5d819
sh0tx420 <sh0tx.cosmoline@gmail.com>
2021-09-26 13:48:25 +0300

    updated rtmp.js

--- src/rtmp.js
+++ src/rtmp.js
@@ -1,14 +1,34 @@
 import * as React from "react";
-import { Box, AppBar, Toolbar, Typography, IconButton, List, ListItemButton, ListItemIcon, ListItemText, Drawer, Divider, Grid, Card, CardContent } from "@mui/material";
+import {
+    Box, AppBar, Toolbar, Typography, IconButton, List, ListItemButton, ListItemIcon, ListItemText, ListItem,
+    Drawer, Divider, Grid, Card, CardContent, CardActions, Collapse, TextField
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import HomeIcon from "@mui/icons-material/Home";
 import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ReactHlsPlayer from "react-hls-player";
+import { styled } from "@mui/material/styles";
+import { Announcement } from "@mui/icons-material";
+
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    expand;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
 
 
 export default function rtmp()
 {
     const [state, setState] = React.useState({});
+    const [expanded, setExpanded] = React.useState(false);
 
     const toggleDrawer = (anchor, open) => (event) => {
         if (event && event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) { 
@@ -16,6 +36,10 @@ export default function rtmp()
         }  // fixes the react re-render bug thing
 
         setState({ ...state, [anchor]: open });
+    };
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
     };
 
     const list = (anchor) => (
@@ -62,21 +86,66 @@ export default function rtmp()
 
             <Box mt={3}/>
 
-            <Grid container spacing={0} direction="column" alignItems="center" justify="center">
+            <Grid container spacing={1} alignItems="center" justify="center" columns={2}>
+                <Grid item sx={{ width: "350px", marginLeft: "14px" }}>
+                    <Card variant="outlined">
+                        <CardContent>
+                            <Typography variant="h4" align="center">Configuration</Typography>
+                            <Divider/>
+                            <Box mt={1.2}/>
+                            <Typography>Insert an URL to a RTMP stream or pick one of our preset channels.</Typography>
+                            <Box mt={0.7}/>
+                            <TextField label="RTMP Link" variant="standard" sx={{ width: "100%" }}/>
+                        </CardContent>
+                        <CardActions disableSpacing>
+                            <List>
+                                <ListItem>
+                                    <ListItemIcon>
+                                        <Announcement/>
+                                    </ListItemIcon>
+                                    <Typography>News</Typography>
+                                    <ExpandMore expand={expanded} onClick={handleExpandClick}>
+                                        <ExpandMoreIcon/>
+                                    </ExpandMore>
+                                </ListItem>
+                            </List>
+                        </CardActions>
+                        <Collapse in={expanded} timeout="auto" unmountOnEXit>
+                            <CardContent>
+                                <List>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            <HomeIcon/>
+                                        </ListItemIcon>
+                                        <ListItemText>sex</ListItemText>
+                                    </ListItemButton>
+                                </List>
+                            </CardContent>
+                        </Collapse>
+                    </Card>
+                </Grid>
+
                 <Grid item sx={{ width: "80%" }}>
                     <Card>
                         <CardContent>
                             <ReactHlsPlayer
                                 src="https://rt-glb.gcdn.co/live/rtnews/playlist.m3u8"
-                                autoPlay={true}
+                                autoPlay={false}
                                 controls={true}
                                 width="100%"
                                 height="auto"
+                                hlsConfig={{
+                                    maxLoadingDelay: 4,
+                                    minAutoBitrate: 0,
+                                    lowLatencyMode: true,
+                                }}
                             />
                         </CardContent>
                     </Card>
                 </Grid>
             </Grid>
+
+            <Box mt={3}/>
         </div>
     );
 }
