001140647be8fc8c425b08462c5841c8dd3bbb5b
sh0tx420 <sh0tx.cosmoline@gmail.com>
2021-10-12 14:43:37 +0300

    https://gamingnet.cf go now

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,16 @@
 import * as React from "react";
-import { Typography } from "@mui/material";
+import { Card, CardContent, Typography } from "@mui/material";
 
 
 export default function Home()
 {
     return(
-        <Typography variant="h1">helo</Typography>
+        <Card sx={{ width: 700, marginLeft: "auto", marginRight: "auto", marginTop: 5 }}>
+            <CardContent>
+                <Typography variant="h4" align="center">
+                    can someone else do this shit for me please
+                </Typography>
+            </CardContent>
+        </Card>
     );
 }
