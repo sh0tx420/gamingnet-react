fde79e6a450ae919a19bacf706a61b29c4cd3274
sh0tx420 <sh0tx.cosmoline@gmail.com>
2021-11-21 21:43:32 +0200

    Updated About card

--- src/Home.js
+++ src/Home.js
@@ -6,9 +6,12 @@ export default function Home()
 {
     return(
         <Card sx={{ width: 700, marginLeft: "auto", marginRight: "auto", marginTop: 5 }}>
+            <Typography variant="h4" align="center" sx={{ background: "repeating-linear-gradient(to left, #121FCF 0%, #08CFCF 100%)", fontFamily: "'Ubuntu', sans-serif" }}>
+                About
+            </Typography>
             <CardContent>
                 <Typography variant="h4" align="center">
-                    can someone else do this shit for me please
+                    sample text
                 </Typography>
             </CardContent>
         </Card>
